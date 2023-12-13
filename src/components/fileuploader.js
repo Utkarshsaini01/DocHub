97ce91d9cb53aec51496337c7d41ad0b4b48d5a0
Utkarsh Saini <sainiutkarsh01@gmail.com>
2023-12-13 22:56:13 +0530

    Initialize firebase and storing file in firebase

--- src/components/fileuploader.js
+++ src/components/fileuploader.js
@@ -1,38 +1,66 @@
 "use client";
 
 import React, { useState } from "react";
-import {DocumentAddIcon, DocumentIcon, TrashIcon, UploadIcon} from '@heroicons/react/outline';
-
-
-
+import {
+  DocumentAddIcon,
+  DocumentIcon,
+  TrashIcon,
+  UploadIcon,
+} from "@heroicons/react/outline";
+import {storage} from '../Config/firebase';
+import { ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage';
 
 
 const FileUploader = () => {
-  const [selectedFile, setSelectedFile] = useState(null);
+    const [file, setFile] = useState(null);
+    const [progress, setProgress] = useState(0);
+    const [error, setError] = useState(null);
+    const [url, setUrl] = useState(null);
 
 
-  const handleFileChange = (event) => {
-    setSelectedFile(event.target.files[0]);
-  };
+    const handleFileChange = (event) => {
+        setFile(event.target.files[0]);
+    };
 
-  const handleFileDelete = () => {
-    setSelectedFile(null);
-  };
+    const handleFileDelete = () => {
+        setFile(null);
+    };
 
-  const handleUpload = () => {
-    
-  }
+    const handleUpload = () => {
+        if (file) {
+          const storageRef = ref(storage, 'uploads/' + file.name);
+          const uploadTask = uploadBytesResumable(storageRef, file);
+      
+          uploadTask.on(
+            'state_changed',
+            (snapshot) => {
+              const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+              setProgress(progress);
+            },
+            (error) => {
+              setError(error.message);
+            },
+            () => {
+              getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                setUrl(downloadURL);
+              });
+            }
+          );
+        } else {
+          setError('Please select a file to upload.');
+        }
+      };
+      
+      
 
   return (
-    
     <div className="border border-0 border-transparent">
-        
-      {!selectedFile ? (
+      {!file ? (
         // No file selected
         <div className="flex flex-col item-center justify-center p-4">
           <div className="box-content bg-slate-300 text-center p-2 rounded flex items-center justify-center">
-          <DocumentIcon className="h-5 w-5 text-blue-500 mx-1" /> 
-          <span>No file selected</span>
+            <DocumentIcon className="h-5 w-5 text-blue-500 mx-1" />
+            <span>No file selected</span>
           </div>
           <div className="box-content bg-sky-300 text-center p-2 rounded mt-2 relative flex items-center justify-center">
             <input
@@ -40,7 +68,7 @@ const FileUploader = () => {
               onChange={handleFileChange}
               className="absolute inset-0 w-full h-full opacity-0 cursor-pointer"
             />
-            <DocumentAddIcon className="h-5 w-5 text-blue-500 mx-1" /> 
+            <DocumentAddIcon className="h-5 w-5 text-blue-500 mx-1" />
             <span>Select File</span>
           </div>
         </div>
@@ -48,15 +76,14 @@ const FileUploader = () => {
         // File selected
         <div className="flex flex-col item-center justify-center p-4">
           <div className="box-content bg-slate-300 text-center p-2 rounded flex items-center justify-center">
-            <DocumentIcon className="h-5 w-5 text-blue-500 mx-1" /> 
-            <span>{selectedFile.name}</span>
+            <DocumentIcon className="h-5 w-5 text-blue-500 mx-1" />
+            <span>{file.name}</span>
           </div>
           <div className="box-content bg-red-200 text-center p-2 rounded mt-2 relative flex items-center justify-center">
             <button
               onClick={handleFileDelete}
               className="absolute inset-0 w-full h-full opacity-0 cursor-pointer"
-            >
-            </button>
+            ></button>
             <TrashIcon className="h-5 w-5 text-red-500 mx-1" />
             <span>Delete file</span>
           </div>
@@ -66,16 +93,15 @@ const FileUploader = () => {
               onChange={handleFileChange}
               className="absolute inset-0 w-full h-full opacity-0 cursor-pointer"
             />
-            <DocumentAddIcon className="h-5 w-5 text-blue-500 mx-1" /> 
+            <DocumentAddIcon className="h-5 w-5 text-blue-500 mx-1" />
             <span>Change File</span>
           </div>
           <div className="box-content bg-green-200 text-center p-2 rounded mt-2 relative flex items-center justify-center">
             <button
               onClick={handleUpload}
               className="absolute inset-0 w-full h-full opacity-0 cursor-pointer"
-            >
-            </button>
-            <UploadIcon className="h-5 w-5 text-green-500 mx-1" /> 
+            ></button>
+            <UploadIcon className="h-5 w-5 text-green-500 mx-1" />
             <span>Upload file</span>
           </div>
         </div>
