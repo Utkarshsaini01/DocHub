ffb1200b8b59812862f94be80aec4a5612fa3431
Utkarsh Saini <sainiutkarsh01@gmail.com>
2023-12-14 13:36:16 +0530

    Add uploading Modal and URL Modal

--- src/components/fileuploader.js
+++ src/components/fileuploader.js
@@ -1,60 +1,126 @@
 "use client";
 
 import React, { useState } from "react";
+import Modal from "react-modal";
 import {
   DocumentAddIcon,
   DocumentIcon,
   TrashIcon,
   UploadIcon,
+  XIcon,
+  ClipboardIcon,
 } from "@heroicons/react/outline";
-import {storage} from '../Config/firebase';
-import { ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage';
+import { storage } from "../Config/firebase";
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 
+// Modal.setAppElement('#__next'); // Set the root element for the modal
 
 const FileUploader = () => {
-    const [file, setFile] = useState(null);
-    const [progress, setProgress] = useState(0);
-    const [error, setError] = useState(null);
-    const [url, setUrl] = useState(null);
+  const [file, setFile] = useState(null);
+  const [progress, setProgress] = useState(0);
+  const [error, setError] = useState(null);
+  const [url, setUrl] = useState(null);
+  const [modalIsOpen, setModalIsOpen] = useState(false);
 
+  const handleFileChange = (event) => {
+    setFile(event.target.files[0]);
+  };
 
-    const handleFileChange = (event) => {
-        setFile(event.target.files[0]);
-    };
+  const handleFileDelete = () => {
+    setFile(null);
+  };
 
-    const handleFileDelete = () => {
-        setFile(null);
-    };
+  const handleUpload = () => {
+    setModalIsOpen(true); // Open the modal
 
-    const handleUpload = () => {
-        if (file) {
-          const storageRef = ref(storage, 'uploads/' + file.name);
-          const uploadTask = uploadBytesResumable(storageRef, file);
-      
-          uploadTask.on(
-            'state_changed',
-            (snapshot) => {
-              const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-              setProgress(progress);
-            },
-            (error) => {
-              setError(error.message);
-            },
-            () => {
-              getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-                setUrl(downloadURL);
-              });
-            }
-          );
-        } else {
-          setError('Please select a file to upload.');
+    if (file) {
+      const storageRef = ref(storage, "uploads/" + file.name);
+      const uploadTask = uploadBytesResumable(storageRef, file);
+
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          setProgress(progress);
+        },
+        (error) => {
+          setError(error.message);
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            setUrl(downloadURL);
+            setModalIsOpen(false); // Close the modal after upload
+          });
         }
-      };
-      
-      
+      );
+    } else {
+      setError("Please select a file to upload.");
+    }
+  };
+
+  const closeModal = () => {
+    setModalIsOpen(false);
+    setFile(null);
+    setUrl(null);
+    setProgress(0);
+  };
+
+  const copyToClipboard = () => {
+    if (url) {
+      navigator.clipboard.writeText(url);
+    }
+  };
 
   return (
     <div className="border border-0 border-transparent">
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        contentLabel="Uploading Modal"
+        className="fixed inset-0 backdrop-blur-sm flex items-center justify-center"
+      >
+        <div className="bg-slate-200 w-96 p-4 rounded-md relative">
+          <XIcon
+            className="h-6 w-6 absolute top-2 right-2 text-red-500 cursor-pointer"
+            onClick={closeModal}
+          />
+          <p className="mb-4">Uploading...</p>
+          <div className="w-full m-1 bg-blue-200 rounded-md overflow-hidden">
+            <div
+              className="bg-blue-500 h-4 rounded-md"
+              style={{ width: `${progress}%` }}
+            ></div>
+          </div>
+        </div>
+      </Modal>
+      {/* URL Modal */}
+      {url && (
+        <Modal
+          isOpen={url !== null}
+          onRequestClose={closeModal}
+          contentLabel="URL Modal"
+          className="fixed inset-0 backdrop-blur-sm flex items-center justify-center"
+        >
+          <div className="bg-slate-200 w-96 p-4 rounded-md relative">
+            <XIcon
+              className="h-6 w-6 absolute top-2 right-2 text-red-500 cursor-pointer"
+              onClick={closeModal}
+            />
+            <div>
+                <p>File uploaded successfully! </p>
+                <div className="flex m-2 item-center">
+                  <div className="bg-slate-50 w-80 overflow-x-auto p-r rounded-md whitespace-nowrap">{url}</div>
+                  <ClipboardIcon
+                    className="h-6 w-6 ml-2 text-blue-500 cursor-pointer"
+                    onClick={copyToClipboard}
+                  />
+                </div>  
+            </div>
+          </div>
+        </Modal>
+      )}
+      
       {!file ? (
         // No file selected
         <div className="flex flex-col item-center justify-center p-4">
@@ -77,7 +143,7 @@ const FileUploader = () => {
         <div className="flex flex-col item-center justify-center p-4">
           <div className="box-content bg-slate-300 text-center p-2 rounded flex items-center justify-center">
             <DocumentIcon className="h-5 w-5 text-blue-500 mx-1" />
-            <span>{file.name}</span>
+            <span>{file.name.length < 20 ? file.name : file.name.slice(0, 20) + "..."}</span>
           </div>
           <div className="box-content bg-red-200 text-center p-2 rounded mt-2 relative flex items-center justify-center">
             <button
